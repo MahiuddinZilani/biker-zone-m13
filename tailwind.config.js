6c1d7fdf62db2cac217110bb0a2b75388d9a3bd3
Zilani <zilani.cse.cou@gmail.com>
2023-07-22 23:31:16 +0600

    biker zone, first commit, daisyui and tailwind, no css

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+// module.exports = {
+//     theme: {
+//         colors: {
+//             'primary': '#E76F51',
+//         },
+//     },
+// }
+// module.exports = {
+//     content: ["./src/**/*.{html,js}"],
+//     theme: {
+//         extend: {
+//             colors: {
+//                 'regal-blue': '#243c5a',
+//               },
+//         },
+//     },
+//     plugins: [],
+// }
+
+
+  
